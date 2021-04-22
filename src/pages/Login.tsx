37c238f97ce05e5baa7163bbc1a827507eb012df
cleison alexandre azevedo pereira <cleison.alexandre.azevedo@gmail.com>
2021-04-22 16:50:19 -0300

    correções

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,82 +1,120 @@
-import React from 'react'
-import { useHistory, Link } from 'react-router-dom'
-import NavbarComponent from '../components/NavBar'
+import React, { useContext } from "react";
+import { useHistory, Link } from "react-router-dom";
+import NavbarComponent from "../components/NavBar";
 import { useForm } from "react-hook-form";
 import api from "../services/api";
+import StoreContext from "../components/Store/Context";
 
 type Inputs = {
-    email: string,
-    password: string,
+  email: string;
+  password: string;
 };
 
-const Login: React.FC = (props) => {
-    const history = useHistory();
-    const { register, handleSubmit, watch, errors } = useForm<Inputs>();
 
-    const onSubmit = async (data: any) => {
-        await api.post('/localhost:8080/login', data)
+
+function login({ email, password }: any) {
+  if (email === "admin@gmail.com" && password === "123") {
+    return { token: "1234" };
+  } else {
+    return { error: "usuario invalido" };
+  }
+}
+
+const Login: React.FC = (props) => {
+  const { register, handleSubmit, watch, errors } = useForm<Inputs>();
+  const history = useHistory();
+  const { setToken } = useContext(StoreContext);
+  const onSubmit = async (data: any) => {
+  
+    //ainda nao ha api
+    /*await api.post('/localhost:8080/login', data)
             .then(function (response) {
                 history.push('/')
             })
             .catch(function (error) {
                 alert(error);
-            });
-
+            });*/
+    const { token } = login(data);
+    if (token) {
+      setToken(token);
+      return history.push("/minhahome");
     }
+      return history.push("/login");
+  };
 
-    return (
-        <>
-            <NavbarComponent title="Best Cloud" link1="PESSOAL" link2="PEQUENAS EMPRESAS" link3="GRANDES EMPRESAS" />
-            <div className="body">
-                <div className="product-panel">
-                    <h2 className="Entrar-tag-form">Entrar</h2>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        {/* register your input into the hook by invoking the "register" function */}
-                        <div className="mb-3 text-product">
-                            <label className="form-label">Email</label>
-                            <input type="email" className="form-control" name="email" ref={register(
-                                {
-                                    required: {
-                                        value: true,
-                                        message: 'Email ou senha inválido'
-                                    },
-                                })} />
-                            {errors.email && <span>{errors.email.message}</span>}
-                        </div>
+  return (
+    <>
+      <NavbarComponent
+        title="Best Cloud"
+        link1="PESSOAL"
+        link2="PEQUENAS EMPRESAS"
+        link3="GRANDES EMPRESAS"
+      />
+      <div className="body">
+        <div className="product-panel">
+          <h2 className="Entrar-tag-form">Entrar</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {/* register your input into the hook by invoking the "register" function */}
+            <div className="mb-3 text-product">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Email ou senha inválido",
+                  },
+                })}
+              />
+              {errors.email && <span>{errors.email.message}</span>}
+            </div>
 
+            <div className="mb-3 text-product">
+              <label className="form-label">Senha</label>
+              {/* include validation with required or other standard HTML validation rules */}
+              <input
+                id="password"
+                type="password"
+                className="form-control"
+                name="password"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: "Email ou senha inválido",
+                  },
+                })}
+              />
+              {/* errors will return when field validation fails  */}
+              {errors.password && <span>{errors.password.message}</span>}
+            </div>
+            <div className="login-button">
+              <button
+                type="submit"
+                className="btn btn-block btn-lg btn-primary"
+              >
+                ENTRAR
+              </button>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value="checked"
+                  id="flexCheckDefault"
+                />
+                <label className="form-check-label">Lembre-se de mim.</label>
+              </div>
+            </div>
+          </form>
+          <p className="text-product">
+            Novo por aqui?{" "}
+            <Link to="/CriarConta">Crie uma conta gratuitamente.</Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-                        <div className="mb-3 text-product">
-                            <label className="form-label">Senha</label>
-                            {/* include validation with required or other standard HTML validation rules */}
-                            <input id="password" type="password" className="form-control" name="password"
-                                ref={register({
-                                    required: {
-                                        value: true,
-                                        message: 'Email ou senha inválido'
-                                    }
-                                })} />
-                            {/* errors will return when field validation fails  */}
-                            {errors.password && <span>{errors.password.message}</span>}
-                        </div>
-                        <div className="login-button">
-                            <button type="submit" className="btn btn-block btn-lg btn-primary">ENTRAR</button>
-                            <div className="form-check">
-                            <input className="form-check-input" type="checkbox" value="checked" id="flexCheckDefault" />
-                                <label className="form-check-label">
-                                    Lembre-se de mim.
-                                </label>
-                            </div>
-                        </div>
-                        
-                            
-                        
-
-                    </form>
-                        <p className="text-product">Novo por aqui? <Link to="/CriarConta">Crie uma conta gratuitamente.</Link></p>
-                </div>
-                </div>
-        </>
-            )
-}
-
-            export default Login;
+export default Login;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import {Route, BrowserRouter} from 'react-router-dom';
-import Home from './pages/Home';
-import CriarConta from './pages/CriarConta';
-import GrandesEmpresas from './pages/GrandesEmpresas';
-import Login from './pages/Login';
-import Logout from './pages/Logout';
-import PequenasEmpresas from './pages/PequenasEmpresas';
-import Pessoal from './pages/Pessoal';
+import React from "react";
+import StoreProvider from "./components/Store/Provider";
+import { Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import CriarConta from "./pages/CriarConta";
+import GrandesEmpresas from "./pages/GrandesEmpresas";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import PequenasEmpresas from "./pages/PequenasEmpresas";
+import Pessoal from "./pages/Pessoal";
+import PrivateRoutes from './privateRoutes'
+import MinhaArea from './pages/MinhaArea'
 
-
-const Routes = () =>{
-    return(
-
-        <BrowserRouter>
-          <Route component={Pessoal} path="/Pessoal"></Route>
-          <Route component={PequenasEmpresas} path="/PequenasEmpresas"></Route>
-          <Route component={GrandesEmpresas} path="/GrandesEmpresas"></Route>
-          <Route component={Login} path="/Login"></Route>
-          <Route component={Logout} path="/Logout"></Route>
-          <Route component={CriarConta} path="/CriarConta"></Route>
-          <Route component={Home} exact path="/" ></Route>
-        </BrowserRouter>
-
-    );
-}
+const Routes = () => {
+  return (
+    <StoreProvider>
+      <BrowserRouter>
+        <Route component={Pessoal} path="/Pessoal"></Route>
+        <Route component={PequenasEmpresas} path="/PequenasEmpresas"></Route>
+        <Route component={GrandesEmpresas} path="/GrandesEmpresas"></Route>
+        <Route component={Login} path="/Login"></Route>
+        <Route component={Logout} path="/Logout"></Route>
+        <Route component={CriarConta} path="/CriarConta"></Route>
+        <Route component={Home} exact path="/"></Route>
+        <PrivateRoutes component={MinhaArea} exact path="/minhahome"></PrivateRoutes>
+      </BrowserRouter>
+    </StoreProvider>
+  );
+};
 
 export default Routes;
